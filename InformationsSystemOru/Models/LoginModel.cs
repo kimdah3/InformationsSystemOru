5f01b3866f3a46fda37e72f433d87b889863e0c1
malmberg <malmberg_d@hotmail.com>
2016-04-05 15:41:02 +0200

    Fixat login

---
+++ InformationsSystemOru/Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace InformationsSystemOru.Models
+{
+    public class LoginModel
+    {
+
+        
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Du måste ange ett användarnamn!")]
+        public string Username { get; set; }
+        
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Du måste ange ett lösenord")]
+        public string Password { get; set; }
+        
+    }
+}
