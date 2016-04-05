75f0faccc21509e45edd0e012c6ab1ad2528b18c
malmberg <malmberg_d@hotmail.com>
2016-04-05 15:43:58 +0200

    synka

--- InformationsSystemOru/Controllers/HomeController.cs
+++ InformationsSystemOru/Controllers/HomeController.cs
@@ -9,27 +9,27 @@ using System.Web.Security;
 
 namespace InformationsSystemOru.Controllers
 {
-//    public class HomeController : Controller
-//    {
-//        [HttpPost]
+    public class HomeController : Controller
+    {
+        [HttpPost]
 
-//        public ActionResult Index(LoginModel model)
-//        {
-           
-//            if (!ModelState.IsValid)
-//                return View(model);
-//            var userRepository = new blblablalRepository();
-//            if (!userRepository.Exists(model.Username, model.Password))
-//            {
-                
-//                return View(model);
-//            }
-//            {
-//                FormsAuthentication.SetAuthCookie(model.Username, false);
-//                return RedirectToAction("blablablaView", "Blablabla", new RouteValueDictionary(new { username = model.Username }));
-//            }
+        public ActionResult Index(LoginModel model)
+        {
 
-//        }
+            if (!ModelState.IsValid)
+                return View(model);
+            var userRepository = new blblablalRepository();
+            if (!userRepository.Exists(model.Username, model.Password))
+            {
 
-//    }
+                return View(model);
+            }
+            {
+                FormsAuthentication.SetAuthCookie(model.Username, false);
+                return RedirectToAction("blablablaView", "Blablabla", new RouteValueDictionary(new { username = model.Username }));
+            }
+
+        }
+
+    }
 }
