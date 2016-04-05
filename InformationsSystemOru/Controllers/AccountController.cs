1334397eed5d6da425c634ff1be9e7d1aca4531e
thommylindahl <thommylindahl@hotmail.com>
2016-04-05 13:13:38 +0200

    create motod

---
+++ InformationsSystemOru/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InformationsSystemOru.Models;
+
+namespace InformationsSystemOru.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult(CreateModel cModel)
+        //{
+        //    users user = new users
+        //    {
+        //        firstname = cModel.Firstname,
+        //        lastname = cModel.Lastname,
+        //        email=cModel.Email
+
+        //     };
+        //    userRepository.AddUser(user);
+        //    return View(cModel);
+        //}
+        
+    }
+}
---
+++ InformationsSystemOru/Models/CreateModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InformationsSystemOru.Models
+{
+    public class CreateModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+    }
+    
+}
