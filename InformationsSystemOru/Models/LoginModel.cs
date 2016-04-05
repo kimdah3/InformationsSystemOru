2ac2e4058614228363b33faf3f8b448b00b805ec
malmberg <malmberg_d@hotmail.com>
2016-04-05 14:34:20 +0200

    Lagt till loginmodel

---
+++ InformationsSystemOru/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InformationsSystemOru.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+        [Display(Name = "PassworSd")]
+        public string Password { get; set; }
+    }
+}
