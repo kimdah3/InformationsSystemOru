c05128d9bae5047cd4ca7c7a3e22ced29b2002f6
LinusIvarsson <ivarsson.linus@gmail.com>
2016-04-06 14:39:33 +0200

    Skapat Controller och klasser för Access-hantering.

---
+++ InformationsSystemOru/Controllers/HandleAccessController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InformationsSystemOru.Controllers
+{
+    public class HandleAccessController : Controller
+    {
+        // GET: HandleAccess
+        public ActionResult HandleAccess()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ InformationsSystemOru/Models/HandleAccessModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InformationsSystemOru.Models
+{
+    public class HandleAccessModel
+    {
+    }
+}
