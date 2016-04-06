1682366bcc03db811de0bd9cb9597abdcad91ceb
kimdah3 <dahlberg.kim20@gmail.com>
2016-04-06 12:21:05 +0200

    Fixat connection string, lagt till accountcontrollertest

---
+++ InformationsSystemOru.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InformationsSystemOru;
+using InformationsSystemOru.Controllers;
+
+namespace InformationsSystemOru.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
