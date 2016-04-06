d58ea242d8aacb3c8148e3d15bce9bae338c74f5
LinusIvarsson <ivarsson.linus@gmail.com>
2016-04-06 11:38:58 +0200

    La till shared views för inloggad, admin samt utloggad användare.

---
+++ Data Access Layer/Repositories/AccessRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Repositories
+{
+  /*  class AccessRepository
+    {
+        public void AddAccess(User user, string accesslevel) {
+            
+            using (var context = new IsOruDbEntities())
+            {
+                context.User_Access.
+            }
+                
+        }
+    }*/
+}
