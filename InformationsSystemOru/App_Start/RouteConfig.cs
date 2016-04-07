a9025cccb4358fa649a79b4cfc4ddb167b3bc8f3
johnsrsly <johnelarssonz@gmail.com>
2016-04-07 13:54:31 +0200

    Fixat det Kim förstörde!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- InformationsSystemOru/App_Start/RouteConfig.cs
+++ InformationsSystemOru/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace InformationsSystemOru
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Calendar", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
