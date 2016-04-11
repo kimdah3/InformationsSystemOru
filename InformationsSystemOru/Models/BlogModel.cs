892427539d97fd376b03afa9eee8d2f07d62d6fa
Lars <lars_kennemark@hotmail.com>
2016-04-11 13:04:00 +0200

    lars anser sig vara klart

--- InformationsSystemOru/Models/BlogModel.cs
+++ InformationsSystemOru/Models/BlogModel.cs
@@ -13,13 +13,11 @@ namespace InformationsSystemOru.Models
         public List<Post> AllPostsForUser { get; set; }
 
         [Display(Name = "Text")]
-        [RegularExpression(@"[a-zA-ZäöåÄÖÅ]+", ErrorMessage = "Text must contain letters (A - Ö)")]
         [Required(ErrorMessage = "Write Something")]
         public string Text { get; set; }
 
 
         [Display(Name = "Title")]
-        [RegularExpression(@"[a-zA-ZäöåÄÖÅ]+", ErrorMessage = "Title must contain letters (A - Ö)")]
         [Required(ErrorMessage = "Enter a Title")]
         public string Title { get; set; } //max 50
 
@@ -27,7 +25,6 @@ namespace InformationsSystemOru.Models
         public DateTime DatePosted { get; set; }
 
         [Display(Name = "Category")]
-        [RegularExpression(@"[a-zA-ZäöåÄÖÅ]+", ErrorMessage = "Category must contain letters (A - Ö)")]
         [Required(ErrorMessage = "Enter a Category")]
         public string Category { get; set; } //rätt? max 10 char
 
