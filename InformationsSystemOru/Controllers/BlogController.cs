a8e71b3b4c97e856c7b7f1dcc95df65afd8de7c4
Oskar Karlsson <oskarkarlsson94@hotmail.com>
2016-04-12 15:40:24 +0200

    Fixade så man kan posta i educationblog

--- InformationsSystemOru/Controllers/BlogController.cs
+++ InformationsSystemOru/Controllers/BlogController.cs
@@ -11,6 +11,8 @@ namespace InformationsSystemOru.Controllers
 {
     public class BlogController : Controller
     {
+        private AccountRepository accountRep = new AccountRepository();
+        private Post_PostTypeRespository postPostType = new Post_PostTypeRespository();
         private PostRepository postrepository;
         // GET: Blog
 
@@ -19,15 +21,45 @@ namespace InformationsSystemOru.Controllers
             postrepository = new PostRepository();
         }
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+        
         public ActionResult InformalBlog()
         {
 
             return View();
+        }
+
+        public ActionResult ScienceBlog()
+        {
+            return View();
+        }
+
+        public ActionResult EducationBlog()
+        {
+            var loggedInUser = accountRep.GetIdFromUsername(User.Identity.Name);
+            //var posts = postrepository.GetProfileBlogPosts(loggedInUser, postPostType.GetAllPrivatePostIds());
+            //return View(new BlogModel { AllPostsForUser = posts });
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EducationBlog(BlogModel model)
+        {
+            var postingUserId = accountRep.GetIdFromUsername(User.Identity.Name);
+
+            var post = new Post()
+            {
+                Category = model.Category,
+                Date = DateTime.Now,
+                Titel = model.Title,
+                Text = model.Text,
+                PostingUserID = postingUserId
+                
+
+            };
+            int type = model.Type;
+            postrepository.SavePost(post, type);
+            postPostType.SavePosttype(post.Id, 3);
+            return RedirectToAction("Educationblog");
         }
 
         //public ActionResult SavePost(BlogModel model)
