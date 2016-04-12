d5b12aed755032e63a87d578038c4756d9dac120
kimdah3 <dahlberg.kim20@gmail.com>
2016-04-12 15:09:06 +0200

    Meeting update 20.10393.1123

--- InformationsSystemOru/Models/MeetingModel.cs
+++ InformationsSystemOru/Models/MeetingModel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Globalization;
 using System.Linq;
 using System.Web;
 using Data_Access_Layer;
@@ -10,20 +8,11 @@ namespace InformationsSystemOru.Models
 {
     public class MeetingModel
     {
-        public List<Meeting> Meetings { get; set; }
-        public string Date { get; set; }
+        public Meeting Meeting { get; set; }
+        public User Host { get; set; }
 
-
-        [Display(Name = "Time")]
-        [Required(ErrorMessage = "Enter a time!")]
-        //[DataType(DataType.DateTime)]
-        public string TimeOfDay { get; set; }
-        [Required(ErrorMessage = "Enter a Location!")]
-        [Display(Name = "Location")]
-        public string Location { get; set; }
-        [Required(ErrorMessage = "Enter a Type!")]
-        [Display(Name = "Type")]
-        public string Type { get; set; }
-
+        public List<User> AllUsers { get; set; }
+        public List<User> InvitedUsers { get; set; }
+        public List<User> AcceptedUsers { get; set; }
     }
 }
---
+++ InformationsSystemOru/Models/MeetingsModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using Data_Access_Layer;
+
+namespace InformationsSystemOru.Models
+{
+    public class MeetingsModel
+    {
+        public string Date { get; set; }
+        public List<Meeting> Meetings { get; set; }
+        public List<User> AllUsers { get; set; }
+        public List<User> InvitedUsers { get; set; }
+
+        [Display(Name = "Time")]
+        [Required(ErrorMessage = "Enter a time!")]
+        //[DataType(DataType.DateTime)]
+        public string TimeOfDay { get; set; }
+        [Required(ErrorMessage = "Enter a Location!")]
+        [Display(Name = "Location")]
+        public string Location { get; set; }
+        [Required(ErrorMessage = "Enter a Type!")]
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+
+    }
+}
